d18ba525b3f0feb3d66a87120b5a3843a8a21e82
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2014-09-03 17:03:16 +0200

    set emotions

---
+++ InstaCar/Data/Skins/SetEmotions/SetEmotions.h
@@ -0,0 +1,17 @@
+//
+//  SetEmotions.h
+//  InstaCar
+//
+//  Created by VRS on 9/3/14.
+//  Copyright (c) 2014 Viktor Sydorenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SkinSetBase.h"
+
+#import "SkinEmo_IHeartLogo.h"
+#import "SkinEmo_IHeartName.h"
+#import "SkinEmo_MyDream.h"
+
+@interface SetEmotions : SkinSetBase
+@end
