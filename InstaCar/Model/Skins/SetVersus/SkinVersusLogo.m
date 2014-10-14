d5a4bc54d33efa4a8908c781b9ae6fc1049fb60d
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2014-10-15 01:54:31 +0200

    Skin Versus Logo constraint fix

--- InstaCar/Model/Skins/SetVersus/SkinVersusLogo.m
+++ InstaCar/Model/Skins/SetVersus/SkinVersusLogo.m
@@ -14,15 +14,9 @@
 @property (weak, nonatomic) IBOutlet SkinElementImage *imgEmblem2;
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintTopMargin;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintHeight;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintWidthLogo1;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintwidthLogo2;
 @property (weak, nonatomic) IBOutlet UIView *movingView;
-
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintLogo1TopMargin;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintLogo1BottomMargin;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintLogo2TopMargin;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintLogo2BottomMargin;
 
 @end
 
@@ -41,33 +35,18 @@
     self.imgEmblem1.contentMode = UIViewContentModeScaleAspectFit;
     [self.imgEmblem1 setImageLogoName:fieldAuto1.logoName];
     self.imgEmblem1.image = fieldAuto1.logo128;
+    
+    CGFloat widthHeightRate = fieldAuto1 ? fieldAuto1.logoWidthHeightRate : 1.0;
+    self.constraintWidthLogo1.constant = widthHeightRate * self.imgEmblem1.bounds.size.height;
 }
 
 -(void)fieldAuto2DidUpdate{
     self.imgEmblem2.contentMode = UIViewContentModeScaleAspectFit;
     [self.imgEmblem2 setImageLogoName:fieldAuto2.logoName];
     self.imgEmblem2.image = fieldAuto2.logo128;
-}
-
--(void)layoutSubviews{
-    // moving view height
-    self.constraintHeight.constant = self.bounds.size.height * heightScaleFactor;
     
-    // logo top & bottom margins
-    CGFloat logosTopBottomMargin = 5.0;//self.movingView.bounds.size.height / 6.0;
-    self.constraintLogo1TopMargin.constant = logosTopBottomMargin;
-    self.constraintLogo1BottomMargin.constant = logosTopBottomMargin;
-    self.constraintLogo2TopMargin.constant = logosTopBottomMargin;
-    self.constraintLogo2BottomMargin.constant = logosTopBottomMargin;
-    
-    // logo proportion
-    CGFloat widthHeightRate = fieldAuto1 ? fieldAuto1.logoWidthHeightRate : 1.0;
-    self.constraintWidthLogo1.constant = widthHeightRate * self.imgEmblem1.bounds.size.height; // adjust place for logo 1
-    
-    widthHeightRate = fieldAuto2 ? fieldAuto2.logoWidthHeightRate : 1.0;
-    self.constraintwidthLogo2.constant = widthHeightRate * self.imgEmblem2.bounds.size.height; // adjust place for logo 2
-    
-    [super layoutSubviews];
+    CGFloat widthHeightRate = fieldAuto2 ? fieldAuto2.logoWidthHeightRate : 1.0;
+    self.constraintwidthLogo2.constant = widthHeightRate * self.imgEmblem2.bounds.size.height;
 }
 
 -(void)onCmdInvertColors{
